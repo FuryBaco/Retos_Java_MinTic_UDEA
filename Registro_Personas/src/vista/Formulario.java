0af6f9c9c4ef10bb6f63d93da2386b12cf392c2d
FuryBaco <jarodriguez5699@misena.edu.co>
2022-07-19 13:26:02 -0500

    update

---
+++ Registro_Personas/src/vista/Formulario.java
@@ -0,0 +1,85 @@
+package vista;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+//Crear una clase que herede de JFrame 
+public class Formulario extends JFrame {
+    //  ATRIBUTOS
+    private JLabel lblNombre;
+    private JLabel lblApellido;
+    private JLabel lblEdad;
+    private JLabel lblSexo;
+    private JLabel lblCedula;
+    private JTextField txtNombre;
+    private JTextField txtApellido;
+    private JTextField txtEdad;
+    private JTextField txtSexo;
+    private JTextField txtCedula;
+    private JButton btnRegistrar;
+
+    //  CONSTRUCTOR
+    public Formulario() {
+        //  Llamar al constructor de la clase padre
+        super();
+        //  Inicializar los atributos
+        lblNombre = new JLabel("Nombre");
+        lblApellido = new JLabel("Apellido");
+        lblEdad = new JLabel("Edad");
+        lblSexo = new JLabel("Sexo");
+        lblCedula = new JLabel("Cedula");
+        txtNombre = new JTextField();
+        txtApellido = new JTextField();
+        txtEdad = new JTextField();
+        txtSexo = new JTextField();
+        txtCedula = new JTextField();
+        btnRegistrar = new JButton("Registrar");
+        //  Agregar los componentes a la ventana
+        lblNombre.setBounds(10, 10, 70, 10);
+        this.add(lblNombre);
+        txtNombre.setBounds(80, 10, 70, 20);
+        this.add(txtNombre);
+        lblApellido.setBounds(10, 30, 70, 10);
+        this.add(lblApellido);
+        txtApellido.setBounds(80, 30, 70, 20);
+        this.add(txtApellido);
+        lblEdad.setBounds(10, 50, 70, 10);
+        this.add(lblEdad);
+        txtEdad.setBounds(80, 50, 70, 20);
+        this.add(txtEdad);
+        lblSexo.setBounds(10, 70, 70, 10);
+        this.add(lblSexo);
+        txtSexo.setBounds(80, 70, 70, 20);
+        this.add(txtSexo);
+        lblCedula.setBounds(10, 90, 70, 10);
+        this.add(lblCedula);
+        txtCedula.setBounds(80, 90, 70, 20);
+        this.add(txtCedula);
+        btnRegistrar.setBounds(80, 150, 140, 20);
+        this.add(btnRegistrar);
+
+        //  Configurar el layout de la ventana
+        this.getContentPane().setLayout(null);
+        //  Configurar el tamaño de la ventana y la ubicacion en el centro de la pantalla
+        this.setBounds(200, 200, 300, 300);
+        //  Configurar el titulo de la ventana
+        this.setTitle("Formulario");
+        //  Hacer que la ventana se cierre al hacer click en el boton cerrar
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //  MANEJADORES DE EVENTOS
+        btnRegistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                //  Poner acciones a realizar cuando se haga click en el boton
+            }
+        });
+
+
+        //  Hacer que la ventana se vea
+        this.setVisible(true);
+    }
+}//fin de la clase Formulario
+
+
