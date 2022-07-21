64b8ecf62b0a6b03614bf78897d6003a8be368eb
FuryBaco <jarodriguez5699@misena.edu.co>
2022-07-21 18:42:25 -0500

    update

--- universidad_gui/src/main/java/com/universidad_gui/universidad_gui/Universidad_gui.java
+++ universidad_gui/src/main/java/com/universidad_gui/universidad_gui/Universidad_gui.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.universidad_gui.universidad_gui;
 
 import com.universidad_gui.universidad_gui.vista.Menu;
 
-/**
- *
- * @author Usuario
- */
+
 public class Universidad_gui {
     public static void main(String[] args) {
         Menu menu = new Menu();
